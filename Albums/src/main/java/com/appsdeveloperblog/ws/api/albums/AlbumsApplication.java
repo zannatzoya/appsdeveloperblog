0e7b1b8a924c53b301b5e989c98d318fc040ed21
zannatzoya <chandrakant.m18@gmail.com>
2021-06-23 13:28:53 +0530

    Fist commint by eclipse

--- Albums/src/main/java/com/appsdeveloperblog/ws/api/albums/AlbumsApplication.java
+++ Albums/src/main/java/com/appsdeveloperblog/ws/api/albums/AlbumsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlbumsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("");
 		SpringApplication.run(AlbumsApplication.class, args);
 	}
 
